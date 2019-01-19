4276846a4910339f423a8b1f30d8b5d1093e6dff
Seth Guberman <sguberman@gmail.com>
2019-01-18 22:44:41 -0500

    complete first pass at led control program

--- 08/ledTest/ledTest.ino
+++ 08/ledTest/ledTest.ino
@@ -1,19 +1,79 @@
+#define DEBUG
+
 #define LED1 11
 #define LED2 10
 
 
-void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW);
+void setup()
+{
+    Serial.begin(9600);
+    
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
 }
 
-void loop() {
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, LOW);
-    delay(500);
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, HIGH);
-    delay(500);
+void loop()
+{
+    int goodInput = -1;
+    int ledCombo = -1;
+    
+    goodInput = getInput(&ledCombo);
+    
+    #ifdef DEBUG
+    Serial.print("goodInput = ");
+    Serial.print(goodInput);
+    Serial.print(" ledCombo = ");
+    Serial.println(ledCombo);
+    #endif
+    
+    if (goodInput)
+    {
+        setLEDs(ledCombo);
+    }
+}
+
+int getInput(int *ledCombo)
+{
+    char c;
+    while (true)
+    {
+        if (Serial.available())
+        {
+            c = Serial.read();
+            if (isdigit(c))
+            {
+                int temp = atoi(c);
+                if (temp >= 0 && temp < 4)
+                {
+                    *ledCombo = temp;
+                    return 1;
+                }
+            } else {
+                return 0;
+            }
+        }
+    }
+}
+
+void setLEDs(int ledCombo)
+{
+    switch (ledCombo)
+    {
+        case 0:
+            digitalWrite(LED1, LOW);
+            digitalWrite(LED2, LOW);
+            break;
+        case 1:
+            digitalWrite(LED1, HIGH);
+            digitalWrite(LED2, LOW);
+            break;
+        case 2:
+            digitalWrite(LED1, LOW);
+            digitalWrite(LED2, HIGH);
+            break;
+        case 3:
+            digitalWrite(LED1, HIGH);
+            digitalWrite(LED2, HIGH);
+            break;
+    }
 }
